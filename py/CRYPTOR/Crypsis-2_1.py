91650c0ff168cfef39a0039a7cd90586c483c03a
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2021-10-29 17:47:05 +0200

    Update Crypsis-2_1.py
    simple-Blowfish

--- py/CRYPTOR/Crypsis-2_1.py
+++ py/CRYPTOR/Crypsis-2_1.py
@@ -13,33 +13,7 @@ iv = urandom(8)
 hashnsalt = blowfish.Cipher(key)
 with open (text, 'rb') as f_file_to_crypt:
     text_block = f_file_to_crypt.read()
-    text_block_2 = bytes()
-data_encryption = hashnsalt.encrypt_cfb(text_block_2, iv)
+data_encryption = hashnsalt.encrypt_cfb(text_block, iv)
 
 text_to_write = data_encryption.decode()
 print (text_to_write)
-# print (data_encryption)
-# with open (output, 'wb') as f_output:
-#      f_output.write(text_to_write)
-
-# i = 0
-# file_to_crypt = input ("file to crypt :")
-# 
-# word_key = ("70611ed00e1fa7da86f17bd15cf384569dd9d342b9810f1d359fd492022d328e1668505db7b1341084b0f06acfeca1f5de473b643544176f97589008693c0f19")
-# # pre_keys = sha256(word_key.encode('utf-8')).digest()
-# # hash_keys = hashlib.sha512(pre_keys)
-# # hash_digest = hash_keys.hexdigest()
-# # print (hash_digest)
-# keys = sha256(word_key.encode('utf-8')).digest()
-# with open (file_to_crypt, 'rb') as f_file_to_crypt:
-#     text_block = f_file_to_crypt.read()
-#     text_block_2 = bytes()
-# for i in range (len(text_block)):
-#     c = text_block[i]
-#     j = i % len(keys)
-#     b = bytes ([c^keys[j]])
-#     text_block_2 = text_block_2 + b
-# with open (output, 'wb') as f_output:
-#     f_output.write(text_block_2)
-# print (text_block)
-# print (type(text_block_2))
